5fa2a578af2313fc516eed69b3c7a7ac79da75c0
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-03-17 15:15:28 -0700

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- *        a comma followed by a space, in ascending order.
- *
- * Return: Always 0.
+ * main - program that prints all possible different combinations of two digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: 0
  */
 int main(void)
 {
-	int digit1, digit2;
+	int i = 48;
+	int j;
+	int cal = 44;
+	int sp = 32;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (; i <= 57; i++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		j = i + 1;
+		for (; j <= 57; j++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 9 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			putchar (i);
+			putchar (j);
+			if (i != 56 || (i == 56 && j != 57))
+			{
+				putchar (cal);
+				putchar (sp);
+			}
 		}
 	}
-
-	putchar('\n');
-
+	putchar ('\n');
 	return (0);
 }

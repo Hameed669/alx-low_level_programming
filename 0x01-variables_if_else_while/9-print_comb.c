5f876bb6c1bfd3bdaa9034245aba4efc82a206d7
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-03-17 15:04:10 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -8,13 +9,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 48; num <= 57; n++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(n);
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;		
 		putchar(',');
 		putchar(' ');
 	}
 
 	putchar('\n');
+
 	return (0);
 }

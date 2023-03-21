502d8fbb6f00911dd0b515c72236fe35efc4e294
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-03-21 10:21:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,18 @@
 /**
  * main - Entry Block
   *Description: prints _putchar followed by a new line
- * Return: 0
+ * Return: always 0
  */
 int main(void)
 {
-	char c[8] = "_putchar";
-
-	int i = 0;
-
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }

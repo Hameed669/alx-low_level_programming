f33c5d5de81defaf7945a3ef3a415d9c1d9ca58f
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-03-30 13:26:06 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,22 +3,23 @@
 /**
  * print_number - prints an integer;
  * @n: integer to be printed;
+ * Return: 0
  */
 void print_number(int n)
 {
 	unsigned int n1;
+	 
+	n1 = n
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
+		n1 = -n;
+	} 
+
+	if (n1 / 10 != 0)
 	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
 		print_number(n1 / 10);
-
+	}	
 	_putchar((n1 % 10) + '0');
 }

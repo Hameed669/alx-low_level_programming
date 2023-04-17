93e215261a89dc8196cafddea15efb4529070887
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-04-17 10:25:36 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MARCO
+#define FUNCTION_LIKE_MARCO
+
+/*
+ * file: 3-function_like_marco.h
+ * desc: Header file that defines a function-like macro ABS(x)
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) :(x))
+#endif

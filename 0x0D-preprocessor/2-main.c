8720da06bf4f06d94aff23b8e32edb3579fa07a3
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-04-17 10:11:20 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * file: 2-main.c
+ * main: print the name of the file of the program 
+ * return : always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

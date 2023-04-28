3df72fcaa66135d40a9a4a2406a8e0a9eaf0ee34
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-04-28 05:32:19 -0700

    0. print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t s = 0;
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (!h->str)
 			printf("[0] (nil)\n");
-
-		count++;
+		else 
+			printf("[&u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
 
-	return (count);
+	return (s);
 }

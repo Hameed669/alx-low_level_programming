2573d8efc11d39d41dc5754958c8b6e9275cd6bd
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-05-09 11:44:39 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,14 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 }
 
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
+
 {
 	int len = 0;
 

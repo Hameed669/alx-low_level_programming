f0bd14e1ed6902e592acb694d648de807421c380
Opeyemi olalere <opeyemiolalere16@gmail.com>
2023-05-11 15:21:24 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,40 +10,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd;
+	int r, w, len = 0;
 
-        if (!filename)
-                return (-1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd == -1)
-        {
-                return (-1);
-        }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-        if (text_content)
-                write(fd, text_content, _strlen(text_content) == -1)
-		{
-			return (1);
-		}
+	r = open(filename, O_WRONLY | O_APPEND);
+	w = write(r, text_content, len);
 
-        close(fd);
-        return (1);
-}
+	if (w == -1 || r == -1)
+		return (-1);
 
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-
-{
-        int len = 0;
-
-        while (s[len])
-                len++;
-
-        return (len);
+	close(r);
+	return (1);
 }
